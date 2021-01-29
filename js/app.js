ac235f8a3182b370c23dae83695c50cb29d59593
aloysious <alicia.todd.army@gmail.com>
2021-01-28 17:59:11 -0800

    second stab at object literals / getting closer / does not populate lists yet

--- js/app.js
+++ js/app.js
@@ -1,44 +1,9 @@
 'use strict';
 
-// console.log('Greetings ALOYSIOUS!');
+console.log('Greetings ALOYSIOUS!');
 
-let myContainer = document.getElementById('container');
+//    Seattle
 
-console.log(myContainer);
-
-// 1. create the ELEMENT
-let section = document.createElement('section');
-// 2. give it CONTENT
-// 3. append to the DOM (to its parent or directly to the DOM)
-//  syntax is parent.appendChild(child)
-myContainer.appendChild(section);
-
-// create the image
-// 1. create the ELEMENT
-let img = document.createElement('img');
-// 2. give it content
-img.src = '/img/cookiePile.jpg';
-img.alt = 'Salmon';
-img.title = 'Classic image of a Salmon in all thier glory';
-// 3.append it to the DOM
-section.appendChild(img);
-
-let article = document.createElement('article');
-section.appendChild(article);
-
-let h3 = document.createElement('h3');
-h3.textContent = 'Pat/s Salmon Cookies';
-article.appendChild(h3);
-
-let p = document.createElement('p');
-p.textContent = 'You need not be Pacific Native Born to appreciate the cookies Pat has crafted, his masterpiece is a global hit. Pleasing the teastebuds of hipsters from Seattle clear across the globe.';
-article.appendChild(p);
-
-let seattle = document.getElementById('seattle');
-// console.log(seattle);
-
-
-// Venue : Seattle
 // 6am: 16 cookies
 // 7am: 20 cookies
 // 8am: 35 cookies
@@ -58,28 +23,49 @@ let seattle = document.getElementById('seattle');
 {/* <ul id="Seattle">Seattle</ul> */ }
 
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let hoursOfOperationArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+// console.log(hoursOfOperationArray);
 
-let dailyTotalCookieSales = 0;
-
-function randomCustomerPerHour() {
-  min = Math.ceil(minimumCustomerEachHour);
-  max = Math.floor(maximumCustomerEachHour);
-  return Math.floor(Math.random() * (maximumCustomerEachHour - minimumCustomerEachHour) + minimumCustomerEachHour);
-}
-// Math.random [inclusive] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
-
-let venue = {
+let seattle = {
   name: 'Seattle',
-  minimumCustomerEachHour: 23,
-  maximumCustomerEachHour: 65,
+  minimumCustomerPerHour: 23,
+  maximumCustomerPerHour: 65,
   avgCookieSalesPerCustomer: 6.3,
   cookiesSoldPerHourArray: [],
-  // a method to calcucalte random number of customers per hour
-  //  a method to calculate and populatenumber of cookies sold per hour array
-  // a method to render the list items.
-  randomCustomerPerHour()
-}
+  dailyTotalCookieSales: 0,
+  // create a METHOD to calcucalte random number of customers per hour.
+  generateRandomCustomerPerHour: function () {
+    return Math.floor(Math.random() * (this.maximumCustomerPerHour - this.minimumCustomerPerHour + 1) + this.minimumCustomerPerHour);
+  // Math.random [inclusive] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  },
+  // create a METHOD to calculate and populate the number of cookies sold per hour array.
+  calculateCookiesSoldPerHour: function () {
+    for (let i = 0; i < hoursOfOperationArray.length; i++) {
+      let currentlHourlyCustomer = this.generateRandomCustomerPerHour();
+      let cookiesSoldCurrentHour = Math.ceil(currentlHourlyCustomer * this.avgCookieSalesPerCustomer);
+      // Need to push the information into the array / get current totals
+      this.cookiesSoldPerHourArray.push(cookiesSoldCurrentHour);
+      this.dailyTotalCookieSales += cookiesSoldCurrentHour;
+      // console.log(`Total Customers > by the hour < = ${currentlHourlyCustomer}, Total cookies sold this hour = ${cookiesSoldCurrentHour}`);
+      // console.log(this.dailyTotalCookieSales);
+    }
+  },
+  // create a METHOD to render the list items to the sales.html page.
+  render: function () {
+    this.calculateCookiesSoldPerHour();
+    // console.log('evidence that i am inside of this render ma jig aka method?');
+    let seattleStorefront = document.getElementById('Seattle');
+    // console.log(seattleStorefront + 'is this thing real?');
+    for (let i = 0; i < hoursOfOperationArray.length; i++) {
+      let li = document.createElement('li');
+      li.textContent = `${hoursOfOperationArray[i]}: ${this.cookiesSoldPerHourArray[i]}`;
+      seattleStorefront.appendChild(li);
+    }
+    let li = document.createElement('li');
+    li.textContent = `Total: ${this.dailyTotalCookieSales}`;
+    seattleStorefront.appendChild(li);
+  },
+};
 
-console.log(venue, hours, randomCustomerPerHour);
+
 
